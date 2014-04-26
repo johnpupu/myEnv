597ad953dc55ffacafd95691df7e676d391d8109
johnpupu <johnpupu@gmail.com>
2014-04-26 14:43:48 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -7,8 +7,10 @@ mv .vim .vim.yours
 mv .tmux.conf .tmux.conf.yours
 mv .tmuxr .tmuxr.yours
 mv .cshrc .cshrc.yours
+mv .svndiff_to_vimdiff .svndiff_to_vimdiff.yours
 ln -s myEnv/.vimrc
 ln -s myEnv/.vim
 ln -s myEnv/.tmux.conf
 ln -s myEnv/.tmuxr
 ln -s myEnv/.cshrc
+ln -s myEnv/.svndiff_to_vimdiff
