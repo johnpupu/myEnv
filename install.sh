ce1e48993a5784fb30963ffb2b5149d3e823c520
root <root@dev.tronice.com>
2015-09-30 17:13:49 +0800

    * 修改 install.sh 以符合 CentOS 使用

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ mv .vim .vim.yours
 mv .tmux.conf .tmux.conf.yours
 mv .tmuxr .tmuxr.yours
 mv .cshrc .cshrc.yours
+mv .tcshrc .tcshrc.yours
 mv .svndiff_to_vimdiff .svndiff_to_vimdiff.yours
 ln -s myEnv/.vimrc
 ln -s myEnv/.vim
@@ -14,3 +15,4 @@ ln -s myEnv/.tmux.conf
 ln -s myEnv/.tmuxr
 ln -s myEnv/.cshrc
 ln -s myEnv/.svndiff_to_vimdiff
+ln -s myEnv/.cshrc .tcshrc
