8bcf4a57a886487d6059eda0d913f38ff5b49fe0
johnpupu <john@tronice.com>
2024-11-04 10:18:02 +0800

    feat: add zstyle:completion

--- .oh-my-zsh/custom/plugins/tronice/tronice.plugin.zsh
+++ .oh-my-zsh/custom/plugins/tronice/tronice.plugin.zsh
@@ -58,3 +58,23 @@ fg() {
         builtin fg %"$@"
     fi
 }
+
+# Do menu-driven completion.
+zstyle ':completion:*' menu select
+
+# Color completion for some things.
+# http://linuxshellaccount.blogspot.com/2008/12/color-completion-using-zsh-modules-on.html
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+
+# formatting and messages
+# http://www.masterzen.fr/2009/04/19/in-love-with-zsh-part-one/
+zstyle ':completion:*' verbose yes
+zstyle ':completion:*:descriptions' format "$fg[yellow]%B--- %d%b"
+zstyle ':completion:*:messages' format '%d'
+zstyle ':completion:*:warnings' format "$fg[red]No matches for:$reset_color %d"
+zstyle ':completion:*:corrections' format '%B%d (errors: %e)%b'
+zstyle ':completion:*' group-name ''
+
+# Completers for my own scripts
+zstyle ':completion:*:*:sstrans*:*' file-patterns '*.(lst|clst)'
+zstyle ':completion:*:*:ssnorm*:*' file-patterns '*.tsv'
